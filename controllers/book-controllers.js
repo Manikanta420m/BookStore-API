c1d41f1a126dca2169a567722bf9881944fa078b
Manikanta420m <manikanta420m@gmail.com>
2026-08-12 18:41:13 +0530

    "controllers"

--- controllers/book-controllers.js
+++ controllers/book-controllers.js
@@ -1,11 +1,58 @@
 const Book=require('../models/book');
 
 const getAllBooks=async(req,res)=>{
-
+   try{
+      const allBooks=await Book.find({});
+      if(allBooks.length>0){
+         res.status(200).json({
+            success : true,
+            message : 'List of books successfully fetched',
+            data : allBooks
+         })
+      }
+      else{
+         res.status(404).json({
+            success : false,
+            message : 'No books found in database',
+         })
+      }
+   }
+   catch(e){
+      console.log(e);
+      res.status(500).json({
+            success : false,
+            message : 'somethong went wrong',
+         })
+   }
 }
+
 const getSingleBookById=async(req,res)=>{
-
+    try{
+      const getCurrBook=req.params.id;
+      const bookDetailsById=await Book.findById(getCurrBook);
+      if(!bookDetailsById){
+         res.status(404).json({
+            success : false,
+            message : 'book with cur Id not found',
+         });
+      }
+      else{
+         res.status(200).json({
+            success : true,
+            message : 'book successfully fetched',
+            data : bookDetailsById
+         });
+      }
+    }
+    catch(e){
+      console.log(e);
+      res.status(500).json({
+            success : false,
+            message : 'somethong went wrong',
+         })
+    }
 }
+
 const addNewBook=async(req,res)=>{
    try{
       const newBookFormData=req.body;
@@ -23,7 +70,7 @@ const addNewBook=async(req,res)=>{
    }
 }
 const updateSingleBook=async(req,res)=>{
-
+   
 }
 const deleteBook=async(req,res)=>{
 
